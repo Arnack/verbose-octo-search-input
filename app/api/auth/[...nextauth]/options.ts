56f8a614dfbe56cfdb00a17bfe1acf6dff252feb
arnack <gri-go-riy@mail.ru>
2023-11-16 04:56:56 +0300

    tocken refreshment added

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,10 @@
-import { LOGIN_ENDPOINT, REFRESH_TOKEN_ENDPOINT, TOCKEN_EXPIRATION_TIMEOUT } from "@/app/service/constants";
-import type { NextAuthOptions } from 'next-auth'
+import {
+  LOGIN_ENDPOINT,
+  REFRESH_TOKEN_ENDPOINT,
+  TOCKEN_EXPIRATION_TIMEOUT,
+} from "@/app/service/constants";
+import type { NextAuthOptions, Session } from "next-auth";
+import axios from "axios";
 import { AdapterUser } from "next-auth/adapters";
 import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -7,106 +12,97 @@ import CredentialsProvider from "next-auth/providers/credentials";
 interface CustomUser extends AdapterUser {
   accessToken: string;
   refreshToken: string;
-  accessTokenExpires: number;
+  expires: number;
 }
 
-async function refreshAccessToken(token: JWT & { refreshToken: string }) {
+const refreshAccessToken = async (token: JWT) => {
   try {
-    const response = await fetch(REFRESH_TOKEN_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ refreshToken: token.refreshToken }),
+    const tokenResponse = await axios.post(REFRESH_TOKEN_ENDPOINT, {
+      refresh_token: token.refreshToken,
     });
-
-    const refreshedTokens = await response.json();
-
-    if (!response.ok) {
-      throw refreshedTokens;
-    }
 
     return {
       ...token,
-      accessToken: refreshedTokens.accessToken,
-      accessTokenExpires: Date.now() + TOCKEN_EXPIRATION_TIMEOUT, 
+      accessToken: tokenResponse.data.access_token,
+      accessTokenExpires: Date.now() + TOCKEN_EXPIRATION_TIMEOUT,
+      refreshToken: tokenResponse.data.refresh_token ?? token.refreshToken,
     };
   } catch (error) {
-    console.error('Error refreshing access token:', error);
+    console.error("Error refreshing access token: ", error);
     return {
       ...token,
       error: "RefreshAccessTokenError",
     };
   }
-}
+};
 
-export const options: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const response = await fetch(LOGIN_ENDPOINT, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            password: credentials?.password,
-            email: credentials?.email,
-          })
+const providers = [
+  CredentialsProvider({
+    name: "Credentials",
+    credentials: {
+      email: { label: "Email", type: "email" },
+      password: { label: "Password", type: "password" },
+    },
+    authorize: async (credentials) => {
+      try {
+        const user = await axios.post(LOGIN_ENDPOINT, {
+          password: credentials?.password,
+          email: credentials?.email,
         });
 
-        const data = await response.json();
-
-        console.log('data >>>>', data);
-        
-
-        return data.access_token ?
-          {
-            ...data,
-            accessToken: data.access_token,
-            refreshToken: data.refresh_token,
-            accessTokenExpires: Date.now() + TOCKEN_EXPIRATION_TIMEOUT,
-          } :
-          null;
-      },
-    }),
-  ],
-
-  
-  callbacks: {
-    async jwt({ token, user } : { token: JWT, user: CustomUser }) {
-      if (user) {
-        return {
-        accessToken: user.accessToken,
-        refreshToken: user.refreshToken,
-        accessTokenExpires: Date.now() + TOCKEN_EXPIRATION_TIMEOUT,
-      } }
-
-      console.log('token >>>>', token);
-      
-
-      if (Date.now() < token.accessTokenExpires) {
-        return token;
+        if (user.data.access_token) {
+          return user.data; // TODO exires should be added
+        }
+        return null;
+      } catch (error) {
+        throw new Error(error as string);
       }
+    },
+  }),
+];
 
-       return refreshAccessToken(token);
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
+const callbacks = {
+  jwt: async ({ token, user }) => {
+    if (user) {
+      console.log("user 67 >>>>", user);
+      // token.accessToken = user.data.accessToken;
+      token.accessToken = (user as CustomUser).accessToken;
+      token.accessTokenExpires = Date.now() + TOCKEN_EXPIRATION_TIMEOUT;
+      token.refreshToken = (user as CustomUser).refreshToken;
+    }
+
+    const shouldRefreshTime = Math.round(
+      token.accessTokenExpiry - 60 * 1000 - Date.now()
+    );
+
+    // If the token is still valid, just return it.
+    if (shouldRefreshTime > 0) {
+      return Promise.resolve(token);
+    }
+    // If the call arrives after 23 hours have passed, we allow to refresh the token.
+    token = refreshAccessToken(token);
+    return Promise.resolve(token);
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  session: async ({ session, token }) => {
+    console.log("session 85 >>>>", session);
+    // Here we pass accessToken to the client to be used in authentication with your API
+    session.accessToken = token.accessToken;
+    session.accessTokenExpiry = token.accessTokenExpiry;
+    session.error = token.error;
+
+    return Promise.resolve(session);
+  },
+};
+
+export const options: NextAuthOptions = {
+  providers,
+  callbacks,
   session: {
     strategy: "jwt",
   },
   pages: {
-    signIn: '/signin',
-    error: '/signin',
-  }
+    signIn: "/signin",
+    error: "/signin",
+    signOut: "/signin",
+  },
 };
--- app/service/constants.ts
+++ app/service/constants.ts
@@ -3,5 +3,5 @@ export const LOGIN_ENDPOINT = `${API_BASE_URL}/login`;
 export const SEARCH_ENDPOINT = `${API_BASE_URL}/search`;
 export const REFRESH_TOKEN_ENDPOINT = `${LOGIN_ENDPOINT}/refresh`;
 
-// there is no access token expiration in the API, so we set it to 10 minutes
-export const TOCKEN_EXPIRATION_TIMEOUT = 60 * 10 * 1000; 
+// there is no access token expiration in the API, so we set it manually
+export const TOCKEN_EXPIRATION_TIMEOUT = 60 * 1 * 1000; 
